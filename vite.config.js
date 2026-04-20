203f9f209d3d30e13d6d153645e8f6dc8e952138
Shivangi Agarwal <shivangi@Shivangis-MacBook-Air.local>
2026-04-20 20:26:05 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/E-Commerce-Product-Explorer-Cart-Management-App",
+  base:"/Project-2/",
 })
